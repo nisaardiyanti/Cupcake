0db1bea8841973da937ca32b055f0e34862b2567
nisaardiyanti <100394858+nisaardiyanti@users.noreply.github.com>
2022-09-29 09:43:36 +0700

    Update MainActivity.kt

--- app/src/main/java/com/example/cupcake/MainActivity.kt
+++ app/src/main/java/com/example/cupcake/MainActivity.kt
@@ -23,19 +23,19 @@ import androidx.navigation.ui.setupActionBarWithNavController
 
 
 /**
- * Aktivitas untuk alur pesanan cupcake.
+ * Aktivitas ini akan mengarahkan untuk alur pesanan cupcake.
  */
 class MainActivity : AppCompatActivity(R.layout.activity_main) {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        // Ambil NavController dari NavHostFragment
+        // Akan mengambil NavController dari NavHostFragment
         val navHostFragment = supportFragmentManager
                 .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
         val navController = navHostFragment.navController
 
-        // Siapkan bilah tindakan untuk digunakan dengan NavController
+        //  uSETUPAction akan digunakan dengan NavController
         setupActionBarWithNavController(navController)
     }
 }
